19f2507f406941b688960fda82ec0acc65a71484
Shaina C <scailles23@gmail.com>
2020-04-24 07:41:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,15 @@
+
 class Anagram
-  
+
   attr_accessor :word
-  
+
   def initialize(word)
     @word = word
   end
-  
-    
-  
-  def match(word_array= %w)
-     word_array.select do |element|
-     
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
     end
   end
-    
-  
-  
 end
